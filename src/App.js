f276f4dcf1101b90bdb153ddb2391dd4498a7dbb
SlavaSid <kilory@mail.ru>
2023-12-12 22:28:18 +0300

    Any message - what you have done since your last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link}  from './components/Link/Link';
+
 
 function App() {
   return (
@@ -7,16 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and savvvve to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello! word</p>
+      <Link link="https://reactjs.org" text="Link to react.org" />
+      <Link link="https://reactjs.org" text="Link to google" />
+         
       </header>
     </div>
   );
---
+++ src/components/Link/Link.js
@@ -0,0 +1,13 @@
+import './Link.css'
+
+
+export const Link = (props) => (
+    <a
+    className="App-link"
+    href={props.link}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {props.text}
+  </a>
+)
